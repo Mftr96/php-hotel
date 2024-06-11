90b76907f8a71e7c4f11bb96367eebcfa7944a93
Mftr96 <matteo.federici96@gmail.com>
2024-06-11 17:13:28 +0200

    spostamento style in cartella e creazione tabelle con bootstrap

--- hotel.php
+++ hotel.php
@@ -47,19 +47,32 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="./style/style.css">
     <title>php-hotel</title>
 </head>
 
 <body>
     <h1>lista hotel</h1>
     <pre>
-        
-        <?php
-        //stampo tramite var_dump
-        var_dump($hotels[0])
-        ?>
+    <?php
+    //stampo array hotels
+    var_dump($hotels)
+    ?>
     </pre>
+    <ul class="row m-0">
+            <?php
+            foreach ($hotels as $hotel) {
+                echo "<li class=' col-4 bg-warning'>". $hotel['name'] . ' ' .  "</li>"; 
+                echo "<li class=' col-4 bg-success'>". $hotel['description']. ' ' . "</li>";
+                echo "<li class=' col-4 bg-primary'>". $hotel['vote']. ' ' . "</li>";
+
+            };
+            ?>
+
+    </ul>
+  
 
 </body>
 
